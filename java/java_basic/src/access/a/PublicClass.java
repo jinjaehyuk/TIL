44146c541e4fe4b50127a0c1df8653dab1017026
jinjayhyuk <kpu9939@gmail.com>
2024-09-01 14:07:23 +0900

    java study

---
+++ java/java_basic/src/access/a/PublicClass.java
@@ -0,0 +1,12 @@
+package access.a;
+
+public class PublicClass {
+    public static void main(String[] args) {
+        PublicClass publicclass = new PublicClass();
+        DefaultClass1 class1 = new DefaultClass1();
+    }
+}
+
+class DefaultClass1{
+
+}
