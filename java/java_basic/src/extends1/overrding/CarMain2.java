07e2e63528b919a3549adf8495a76dacbac3377b
jinjayhyuk <kpu9939@gmail.com>
2024-10-02 21:38:37 +0900

    java study

---
+++ java/java_basic/src/extends1/overrding/CarMain2.java
@@ -0,0 +1,13 @@
+package extends1.overrding;
+
+public class CarMain2 {
+    public static void main(String[] args) {
+        ElectricCar electricCar = new ElectricCar();
+        electricCar.move();
+
+        GasCar gasCar = new GasCar();
+        gasCar.move();
+        
+
+    }
+}
